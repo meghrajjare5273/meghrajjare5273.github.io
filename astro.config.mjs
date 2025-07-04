6baaee47ddb8d8ee0aac315bc333f4c07d244a2f
Meghraj Jare <meghrajjare77@gmail.com>
2025-07-04 16:49:45 +0530

    Test Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://meghrajjare5273.github.io",
   base: "/",
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
